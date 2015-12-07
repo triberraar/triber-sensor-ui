51b2bd1742d16a512af87444ed074d7f2e3fa8da
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-12-07 20:32:49 +0100

    font awesome stuff

--- public/src/app.js
+++ public/src/app.js
@@ -1,3 +1,20 @@
+'use strict';
+
+import 'angular-material/angular-material.css';
+import 'font-awesome/css/font-awesome.css';
+import './app.css';
+
 import angular from 'angular';
 
-angular.module('app', []);
+import angularMaterial from 'angular-material';
+import angularUIRouter from 'angular-ui-router';
+
+let dependencies = [
+    angularMaterial,
+    angularUIRouter
+];
+
+const app = angular.module('app', dependencies);
+
+export default app;
+
