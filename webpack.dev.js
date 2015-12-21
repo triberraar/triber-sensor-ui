137c62ef84fa5c627291ea227fd3b7afad1f1ecf
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-12-21 19:41:42 +0100

    config files inherit from each other added a banner

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,68 +1,71 @@
 'use strict';
 
 // Modules
-var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var Clean = require('clean-webpack-plugin');
 var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+var webpack = require('webpack');
+var packageJson = require('./package.json');
 
-var config = {};
+var config = {
+    entry: {
+        app: './public/src/app.js'
+    },
+    output: {
+        path: __dirname + '/public/build',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        preLoaders: [
+            {test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/}
+        ],
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/
+        }, {
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+            loader: 'file'
+        }, {
+            test: /\.html$/,
+            loader: 'raw'
+        }, {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/font-woff'
+        }, {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/font-woff'
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/octet-stream'
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file'
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=image/svg+xml'
+        },{
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    },
+    plugins:[
+        new Clean(['./public/build']),
+        new ngAnnotatePlugin({
+            add: true
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/src/index.html',
+            inject: 'body'
+        }),
+        new webpack.BannerPlugin('' +
+            '**************************************************************\n' +
+            ' ' + packageJson.name + ' build by webpack. version: ' + packageJson.version + '\n' +
+            '**************************************************************\n'
 
-config.entry = {
-    app: './public/src/app.js'
-};
-
-config.output = {
-    path: __dirname + '/public/build',
-    filename: 'bundle.js'
-};
-
-config.devtool = 'source-map';
-
-config.module = {
-    loaders: [{
-        test: /\.js$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
-    }, {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-        loader: 'file'
-    }, {
-        test: /\.html$/,
-        loader: 'raw'
-    }, {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-    }, {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-    }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-    }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-    }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-    },{
-        test: /\.css$/,
-        loader: 'style!css'
-    }
+        )
     ]
 };
-
-config.plugins = [
-    new Clean(['./public/build']),
-    new ngAnnotatePlugin({
-        add: true
-    }),
-    new HtmlWebpackPlugin({
-        template: './public/src/index.html',
-        inject: 'body'
-    })
-];
-
 
 module.exports = config;
--- webpack.test.js
+++ webpack.test.js
@@ -1,65 +1,21 @@
 'use strict';
 
 // Modules
-var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
-var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-var Clean = require('clean-webpack-plugin');
-
-var config = {};
+var config = require('./webpack.dev');
 
 config.entry = {};
 config.output = {};
 
 config.devtool = 'inline-source-map';
-config.module = {
-    loaders: [{
-        test: /\.js$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
-    }, {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-        loader: 'file'
-    }, {
-        test: /\.html$/,
-        loader: 'raw'
-    }, {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-    }, {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-    }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-    }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-    }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-    }, {
-        test: /\.css$/,
-        loader: 'style!css'
-    }
-    ]
-};
 
-config.module.preLoaders=[{
+config.module.preLoaders.push({
     test: /\.js$/,
     exclude: [
         /node_modules/,
         /\.spec\.js$/
     ],
     loader: 'isparta-instrumenter'
-}];
-
-config.plugins = [
-    new Clean(['./public/build']),
-    new ngAnnotatePlugin({
-        add: true
-    })
-];
+});
 
 config.progress = true;
 
